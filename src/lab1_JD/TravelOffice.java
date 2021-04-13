23021ebb2e9ad16435128ec5aa76f422fa08139b
Jakub Dutkiewicz <jak.dut@gmail.com>
2021-04-13 08:21:31 +0200

    Refactor TravelOffice class, added constructor, getter and toString. Added addCustomer and getCustomerInfo method.

--- src/lab1_JD/TravelOffice.java
+++ src/lab1_JD/TravelOffice.java
@@ -1,18 +1,51 @@
 package lab1_JD;
 
+import java.util.Arrays;
+
 public class TravelOffice {
 
     private Customer[] customerTab = new Customer[2];
-    private boolean isFull = false;
 
-    public void addCustomer(Customer customer){
-        for (int i = 0; i < customerTab.length; i++) {
-            if (customerTab[i] == null){
-                isFull = true;
+    public TravelOffice(Customer[] customerTab) {
+        this.customerTab = customerTab;
+    }
+
+    public TravelOffice() {
+    }
+
+    public Customer[] getCustomerTab() {
+        return customerTab;
+    }
+
+    @Override
+    public String toString() {
+        return "TravelOffice{" +
+                "customerTab=" + Arrays.toString(customerTab) +
+                '}';
+    }
+
+    public void addCustomer(Customer customer) {
+        if (customerTab[customerTab.length-1] != null) {
+            Customer[] extendedCustomerTable = customerTab;
+            customerTab = new Customer[customerTab.length * 2];
+            for (int i = 0; i < extendedCustomerTable.length; i++) {
+                customerTab[i] = extendedCustomerTable[i];
+                customerTab[extendedCustomerTable.length] = customer;
             }
-            if (!isFull){
-                customerTab = new Customer[customerTab.length + 2];
+        } else {
+            for (int i = 0; i < customerTab.length; i++) {
+                if (customerTab[i] == null)
+                    customerTab[i] = customer;
             }
         }
+    }
+
+    public String getCustomersInfo() {
+        StringBuffer newString = new StringBuffer();
+        for (int i = 0; i < customerTab.length; i++) {
+            newString.append(customerTab[i]);
+        }
+        String str = newString.toString();
+        return str;
     }
 }
